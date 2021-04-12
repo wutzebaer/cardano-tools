8265c82edaa4f4de8f3fd95c9fab3dafadc20b73
peter <petergrossmann@email.de>
2021-04-12 09:34:18 +0200

    init spring boot

---
+++ src/main/java/de/peterspace/cardanominter/CardanoMinterApplication.java
@@ -0,0 +1,13 @@
+package de.peterspace.cardanominter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardanoMinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardanoMinterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/peterspace/cardanominter/CardanoMinterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.peterspace.cardanominter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardanoMinterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
