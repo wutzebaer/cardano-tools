e42f3b4bbd1bb8125c00b18a1a206f9ba8ff4e79
peter <petergrossmann@email.de>
2021-04-13 20:38:00 +0200

    transactions

--- src/main/java/de/peterspace/cardanominter/cli/CardanoCli.java
+++ src/main/java/de/peterspace/cardanominter/cli/CardanoCli.java
@@ -1,5 +1,6 @@
 package de.peterspace.cardanominter.cli;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
@@ -24,33 +25,69 @@ import de.peterspace.cardanominter.annotations.AnnotationHelper;
 @Validated
 public class CardanoCli {
 
-	@Value("#{'${cardano.cli.path}'.split(' ')}")
-	private String[] cardanoCliPath;
+	// cardano.cli.path=docker run --rm -v testnet-ipc:/ipc -v
+	// C:\\github\\cardano-minter\\cardano-minter-workdir:/workdir -w /workdir -e
+	// CARDANO_NODE_SOCKET_PATH=/ipc/node.socket --entrypoint cardano-cli
+	// inputoutput/cardano-node
+	// cat.path=docker run --rm -v
+	// C:\\github\\cardano-minter\\cardano-minter-workdir:/workdir -w /workdir
+	// --entrypoint cat inputoutput/cardano-node
 
-	@Value("#{'${cat.path}'.split(' ')}")
-	private String[] catPath;
+	private String[] cardanoCliCmd;
+	private String[] catCmd;
+	private String[] networkMagicCmd;
 
-	@Value("#{'${network.magic}'.split(' ')}")
-	private String[] networkMagic;
+	@Value("${cardano.node.socket.volume}")
+	private String cardanoNodeSocketVolume;
+
+	@Value("${cardano.node.socket.path}")
+	private String cardanoNodeSocketPath;
+
+	@Value("${working.dir}")
+	private String workingDir;
+
+	@Value("${network.magic}")
+	private String networkMagic;
 
 	@PostConstruct
 	public void init() throws Exception {
+
+		// @formatter:off
+		cardanoCliCmd = new String[] {
+			"docker", "run", "--rm",
+			"-v", cardanoNodeSocketVolume + ":/ipc",
+			"-v", workingDir + ":/workdir",
+			"-w", "/workdir",
+			"-e", "CARDANO_NODE_SOCKET_PATH=" + cardanoNodeSocketPath,
+			"--entrypoint", "cardano-cli",
+			"inputoutput/cardano-node"
+		};
+		catCmd = new String[] {
+			"docker", "run", "--rm",
+			"-v", workingDir + ":/workdir",
+			"-w", "/workdir",
+			"--entrypoint", "cat",
+			"inputoutput/cardano-node"
+		};
+		networkMagicCmd = networkMagic.split(" ");
+		// @formatter:on
+
 		ArrayList<String> cmd = new ArrayList<String>();
-		cmd.addAll(List.of(cardanoCliPath));
+		cmd.addAll(List.of(cardanoCliCmd));
 		cmd.add("query");
 		cmd.add("protocol-parameters");
 		cmd.add("--out-file");
 		cmd.add("protocol.json");
-		cmd.addAll(List.of(networkMagic));
+		cmd.addAll(List.of(networkMagicCmd));
 		runCommand(cmd.toArray(new String[0]));
 	}
 
 	public long getTip() throws Exception {
 		ArrayList<String> cmd = new ArrayList<String>();
-		cmd.addAll(List.of(cardanoCliPath));
+		cmd.addAll(List.of(cardanoCliCmd));
 		cmd.add("query");
 		cmd.add("tip");
-		cmd.addAll(List.of(networkMagic));
+		cmd.addAll(List.of(networkMagicCmd));
 		String jsonString = runCommand(cmd.toArray(new String[0]));
 		JSONObject jsonObject = new JSONObject(jsonString);
 		return jsonObject.getLong("slot");
@@ -61,7 +98,7 @@ public class CardanoCli {
 		String key = UUID.randomUUID().toString();
 
 		ArrayList<String> cmd = new ArrayList<String>();
-		cmd.addAll(List.of(cardanoCliPath));
+		cmd.addAll(List.of(cardanoCliCmd));
 		cmd.add("address");
 		cmd.add("key-gen");
 		cmd.add("--verification-key-file");
@@ -71,14 +108,14 @@ public class CardanoCli {
 		runCommand(cmd.toArray(new String[0]));
 
 		cmd = new ArrayList<String>();
-		cmd.addAll(List.of(cardanoCliPath));
+		cmd.addAll(List.of(cardanoCliCmd));
 		cmd.add("address");
 		cmd.add("build");
 		cmd.add("--payment-verification-key-file");
 		cmd.add(key + ".vkey");
 		cmd.add("--out-file");
 		cmd.add(key + ".addr");
-		cmd.addAll(List.of(networkMagic));
+		cmd.addAll(List.of(networkMagicCmd));
 		runCommand(cmd.toArray(new String[0]));
 
 		return key;
@@ -89,12 +126,12 @@ public class CardanoCli {
 		String address = readAddress(key);
 
 		ArrayList<String> cmd = new ArrayList<String>();
-		cmd.addAll(List.of(cardanoCliPath));
+		cmd.addAll(List.of(cardanoCliCmd));
 		cmd.add("query");
 		cmd.add("utxo");
 		cmd.add("--address");
-		cmd.add(address.trim());
-		cmd.addAll(List.of(networkMagic));
+		cmd.add(address);
+		cmd.addAll(List.of(networkMagicCmd));
 		cmd.add("--out-file");
 		cmd.add(key + ".utxo");
 		runCommand(cmd.toArray(new String[0]));
@@ -124,7 +161,7 @@ public class CardanoCli {
 
 	private String readFile(String filename) throws Exception {
 		ArrayList<String> cmd = new ArrayList<String>();
-		cmd.addAll(List.of(catPath));
+		cmd.addAll(List.of(catCmd));
 		cmd.add(filename);
 		return runCommand(cmd.toArray(new String[0]));
 	}
@@ -133,9 +170,9 @@ public class CardanoCli {
 		Process process = Runtime.getRuntime().exec(cmd);
 		int returnCode = process.waitFor();
 		if (returnCode != 0) {
-			throw new Exception(new String(process.getErrorStream().readAllBytes()));
+			throw new Exception(new String(process.getErrorStream().readAllBytes()).trim());
 		} else {
-			return new String(process.getInputStream().readAllBytes());
+			return new String(process.getInputStream().readAllBytes()).trim();
 		}
 	}
 
---
+++ src/main/java/de/peterspace/cardanominter/cli/CardanoTransaction.java
@@ -0,0 +1,8 @@
+package de.peterspace.cardanominter.cli;
+
+import java.util.List;
+
+@Data
+public class CardanoTransaction {
+	private List<String> in;
+}
