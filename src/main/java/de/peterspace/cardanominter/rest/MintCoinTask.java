cb9f2d265ecc4bf499c2c58a428402b1d68746db
peter <petergrossmann@email.de>
2021-04-16 13:53:40 +0200

    minttask pojo

---
+++ src/main/java/de/peterspace/cardanominter/rest/MintCoinTask.java
@@ -0,0 +1,10 @@
+package de.peterspace.cardanominter.rest;
+
+import java.util.Map;
+
+public class MintCoinTask {
+	private String receiver;
+	private String tokenName;
+	private Long tokenAmount;
+	private Map<String, String> metaData;
+}
