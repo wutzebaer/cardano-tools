390aa27bebfafa7c4c238e9fe3194c3778c03d5c
peter <petergrossmann@email.de>
2021-08-28 20:59:54 +0200

    sort search after tokenname

--- src/main/java/de/peterspace/cardanotools/dbsync/CardanoDbSyncClient.java
+++ src/main/java/de/peterspace/cardanotools/dbsync/CardanoDbSyncClient.java
@@ -441,7 +441,7 @@ public class CardanoDbSyncClient {
 			if (fromMintid != null)
 				findTokenQuery += "and mintid > ? ";
 
-			findTokenQuery += "order by mintid ";
+			findTokenQuery += "order by tokenname ";
 			findTokenQuery += "limit 100 ";
 
 			PreparedStatement getTxInput = connection.prepareStatement(findTokenQuery);
