aee1304969ac3daa127b967a2ff9722976b141c4
peter <petergrossmann@email.de>
2023-05-04 10:23:21 +0200

    fetch prices later

--- src/main/java/de/peterspace/cardanotools/service/PriceService.java
+++ src/main/java/de/peterspace/cardanotools/service/PriceService.java
@@ -33,7 +33,7 @@ public class PriceService {
 	private final Map<LocalDate, Price> prices = new HashMap<>();
 
 	@Scheduled(initialDelay = 0, fixedDelay = Long.MAX_VALUE)
-	@Scheduled(cron = "0 0 * * * *")
+	@Scheduled(cron = "0 3 * * * *")
 	public void createSnapshot() throws MalformedURLException, IOException, InterruptedException {
 		log.info("Creating Snapshot");
 		LocalDate currentDate = priceRepository
