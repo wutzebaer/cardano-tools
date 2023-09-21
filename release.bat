b0daaf9d70b921dca78a3f0dc10b963b09935d19
peter <petergrossmann@email.de>
2023-09-21 14:38:32 +0200

    ignore tests for release

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
-call mvn spring-boot:build-image -Dspring-boot.build-image.imageName=wutzebaer/cardano-tools:latest -DskipTests=true
+call mvn spring-boot:build-image -Dspring-boot.build-image.imageName=wutzebaer/cardano-tools:latest -Dmaven.test.skip
 docker push wutzebaer/cardano-tools:latest
 pause
